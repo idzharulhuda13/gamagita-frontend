755018b35a12756b6707af86f1986f7b92eee822
idzharulhuda13 <idzharul.huda@gmail.com>
2021-05-10 13:47:52 +0700

    master

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -5,37 +5,37 @@
             <div class="insta-item set-bg" data-setbg="img/insta-1.jpg" style="background-image: url('/img/gamagita-1.jpg')">
                 <div class="inside-text">
                     <i class="ti-instagram"></i>
-                    <h5><a href="#">gamagita.id</a></h5>
+                    <h5><a href="https://www.instagram.com/gamagita.id/">gamagita.id</a></h5>
                 </div>
             </div>
             <div class="insta-item set-bg" style="background-image: url('/img/gamagita-2.jpg')">
                 <div class="inside-text">
                     <i class="ti-instagram"></i>
-                    <h5><a href="#">gamagita.id</a></h5>
+                    <h5><a href="https://www.instagram.com/gamagita.id/">gamagita.id</a></h5>
                 </div>
             </div>
             <div class="insta-item set-bg" style="background-image: url('/img/gamagita-6.jpg')">
                 <div class="inside-text">
                     <i class="ti-instagram"></i>
-                    <h5><a href="#">gamagita.id</a></h5>
+                    <h5><a href="https://www.instagram.com/gamagita.id/">gamagita.id</a></h5>
                 </div>
             </div>
             <div class="insta-item set-bg" style="background-image: url('/img/gamagita-3.jpg')">
                 <div class="inside-text">
                     <i class="ti-instagram"></i>
-                    <h5><a href="#">gamagita.id</a></h5>
+                    <h5><a href="https://www.instagram.com/gamagita.id/">gamagita.id</a></h5>
                 </div>
             </div>
             <div class="insta-item set-bg" style="background-image: url('/img/gamagita-4.jpg')">
                 <div class="inside-text">
                     <i class="ti-instagram"></i>
-                    <h5><a href="#">gamagita.id</a></h5>
+                    <h5><a href="https://www.instagram.com/gamagita.id/">gamagita.id</a></h5>
                 </div>
             </div>
             <div class="insta-item set-bg" style="background-image: url('/img/gamagita-5.jpg')">
                 <div class="inside-text">
                     <i class="ti-instagram"></i>
-                    <h5><a href="#">gamagita.id</a></h5>
+                    <h5><a href="https://www.instagram.com/gamagita.id/">gamagita.id</a></h5>
                 </div>
             </div>
         </div>
